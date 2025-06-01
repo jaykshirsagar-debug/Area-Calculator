16282f51a447a557281522d4d8e3ff0a8e6812ed
105912265 <105912265@student.swin.edu.au>
2025-06-01 21:48:51 +1000

    completed

---
+++ calculator.py
@@ -0,0 +1,41 @@
+import math
+
+print('==================')
+print('Area Calculator 📐')
+print('==================')
+
+area = 0
+valid = 0
+
+while valid != 1:
+    print('\nChoose Shape:')
+    print('1. Square')
+    print('2. Rectangle')
+    print('3. Triangle')
+    print('4. Circle')
+
+    print('\nWhich shape: ')
+
+    answer = int(input())
+
+    if answer < 1 or answer > 4:
+        print('Invalid choice!')
+    else:
+        valid = 1
+
+if answer == 1:
+    side = int(input('\nEnter side in metres: ')) 
+    area = side ** 2
+elif answer == 2:
+    length = int(input('\nEnter length in metres: ')) 
+    width = int(input('Enter width in metres: ')) 
+    area = length * width
+elif answer == 3:
+    base = int(input('\nEnter base in metres: ')) 
+    height = int(input('Enter height in metres: ')) 
+    area = base * height * 0.5
+else:
+    radius = int(input('\nEnter radius in metres: '))
+    area = math.pi * (radius ** 2)
+    
+print('\nThe area is ', area, 'm^2')
